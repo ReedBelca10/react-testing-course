68ca2e9158f9ea47cd0cdd3afb8ce1a7491ba328
ReedBelca <reedbelca55@gmail.com>
2026-05-28 16:02:16 +0000

    Testing birthday-reminder app

--- birthday-reminder/wdio.conf.js
+++ birthday-reminder/wdio.conf.js
@@ -1,56 +1,24 @@
-const visualOptions = {
-	apiKey: process.env.SCREENER_API_KEY,
-	projectName: 'ctm2022',
-};
-const sauceOptions = {
-	username: process.env.SAUCE_USERNAME,
-	accesskey: process.env.SAUCE_ACCESS_KEY,
-	build: `Visual Tests-${new Date().getTime()}`,
-	//enable HAR files
-	extendedDebugging: true,
-	capturePerformance: true,
-};
 exports.config = {
-	region: process.env.REGION || 'us',
-	services: [
-		[
-			'sauce',
-			{
-				sauceConnect: true,
-			},
-		],
-	],
+	runner: 'local',
 	specs: ['./test/specs/**/*.js'],
 	// Patterns to exclude.
 	exclude: [
 		// 'path/to/excluded/files'
 	],
-	maxInstances: 10,
-	hostname: 'hub.screener.io',
-	port: 443,
-	protocol: 'https',
-	path: '/wd/hub',
+	maxInstances: 1,
 	capabilities: [
-		//Desktop A 28%: https://www.w3schools.com/browsers/browsers_display.asp
 		{
 			browserName: 'chrome',
-			platformName: 'windows 10',
-			browserVersion: 'latest',
-			'sauce:options': {
-				...sauceOptions,
-			},
-			'sauce:visual': {
-				...visualOptions,
-				viewportSize: '1366x768',
-			},
 		},
 	],
 	//
 	// ===================
 	// Test Configurations
 	// ===================
+	services: ['chromedriver'],
+	chromeDriverArgs: ['--verbose'],
 	// Level of logging verbosity: trace | debug | info | warn | error | silent
-	logLevel: 'error',
+	logLevel: 'info',
 	bail: 0,
 	baseUrl: 'http://localhost:3000',
 	waitforTimeout: 10000,
@@ -60,6 +28,6 @@ exports.config = {
 	reporters: ['spec'],
 	mochaOpts: {
 		ui: 'bdd',
-		timeout: 180000,
+		timeout: 60000,
 	},
 };
